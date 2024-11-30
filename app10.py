fd1ed78237032002f3368ea1b2d1769eabaf39d3
Victor Maciel <victormaciel.dev@gmail.com>
2024-11-30 19:42:43 -0300

    up

---
+++ app10.py
@@ -0,0 +1,11 @@
+# Como navegar até um site com Pyautogui
+import webbrowser
+
+#Abrir uma aba no navegador existente ou criar outro
+webbrowser.open('https://google.com')
+
+# Abrir uma nova aba em um navegador existente ou criar outro
+webbrowser.open_new_tab('https://google.com')
+
+# Criar e abrir uma nova janela do zero
+webbrowser.open_new('https://google.com')
