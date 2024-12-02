c3c0154c57bc8dba7aa860e88a48d419e333de90
Victor Maciel <victormaciel.dev@gmail.com>
2024-12-02 15:02:37 -0300

    up

---
+++ guitarflash.py
@@ -0,0 +1,15 @@
+# feito no notebook
+import pyautogui
+import keyboard
+from time import sleep
+
+while keyboard.is_pressed('1') == False:
+    if pyautogui.pixelMatchesColor(1291,760,(12,152,33)):
+        pyautogui.press('a')
+        sleep(0.05)
+    if pyautogui.pixelMatchesColor(1378,761,(254,15,23)):
+        pyautogui.press('s')
+        sleep(0.05)
+    if pyautogui.pixelMatchesColor(1378,761,(254,15,23)):
+        pyautogui.press('j')
+        sleep(0.05)
