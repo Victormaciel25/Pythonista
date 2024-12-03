b9353f2a0b127c516ad39d3a1edacc2d7353ae9e
Victor Maciel <victormaciel.dev@gmail.com>
2024-12-03 15:23:28 -0300

    Update app.py

--- selenium/app.py
+++ selenium/app.py
@@ -1,41 +1,55 @@
 # selenium 4
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-chrome_options = Options()
-# Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
+def iniciar_driver():
+    chrome_options = Options()
+    # Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
 
-arguments = ['--lang=pt-BR', '--window-size=500,500', '--incognito']
+    arguments = ['--lang=pt-BR', '--window-size=500,500', '--incognito']
 
-'''  
-    Common arguments
---start-maximized # Inicia maximizado
---lang=pt-BR # Define o idioma de inicialização, # en-us, pt-BR
---incognito # Usar o modo anônimo
---window-size=800,800 # Define a resolução da janela em largura e altura
---headless # Roda em segunda plano(com a janela fechada)
---disable-notifications # Desabilita notificações
---disable-gpu # Desabilita renderização com GPU
-'''
+    '''  
+        Common arguments
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-us, pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segunda plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
 
-for argument in arguments:
-    chrome_options.add_argument(argument)
+    for argument in arguments:
+        chrome_options.add_argument(argument)
 
-caminho_padrao_para_download = 'D:\\Downloads'
+    caminho_padrao_para_download = 'D:\\Downloads'
 
-# Lista de opções experimentais (nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3/chrome/common/pref_names.cc
-chrome_options.add_experimental_option("prefs", {
-    'download.default_directory': caminho_padrao_para_download,
-    # Atualiza diretório para diretório passado acima
-    'download.directory_upgrade':True,
-    # Seta se o navegador deve pedir ou não para fazer download
-    'download.prompt_for_download': False,
-    "profile.default_content_setting_values.notifications":2, # Desabilita notificações
-    # Permite realizar múltiplos downloads
-    "profile.default_content_setting_values.automatic_downloads":1,
-})
+    # Lista de opções experimentais (nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3/chrome/common/pref_names.cc
+    chrome_options.add_experimental_option("prefs", {
+        'download.default_directory': caminho_padrao_para_download,
+        # Atualiza diretório para diretório passado acima
+        'download.directory_upgrade':True,
+        # Seta se o navegador deve pedir ou não para fazer download
+        'download.prompt_for_download': False,
+        "profile.default_content_setting_values.notifications":2, # Desabilita notificações
+        # Permite realizar múltiplos downloads
+        "profile.default_content_setting_values.automatic_downloads":1,
+    })
 
-driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
 
-driver.get('https://devaprender.com')
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+
+botao = driver.find_element(By.ID, 'buttonalerta')
+botoes = driver.find_elements(By.ID, 'buttonalerta')
+
+if botao is not None:
+    print('botao foi encontrado')
+if botoes is not None:
+    print('botões encontrados')
+
 input('')
+driver.close()
