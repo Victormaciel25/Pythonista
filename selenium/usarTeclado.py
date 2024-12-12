69e61db43f3e51a413e57247478e0352d95166e0
Victor Maciel <victormaciel.dev@gmail.com>
2024-12-11 21:17:01 -0300

    Create usarTeclado.py

---
+++ selenium/usarTeclado.py
@@ -0,0 +1,36 @@
+from tabnanny import check
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+import random
+from selenium.webdriver.common.keys import Keys
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,1000', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app')
+
+botao_windows = driver.find_element(By. ID, 'WindowsRadioButton')
+botao_windows.click()
+botao_windows.send_keys(Keys.DOWN)
+
+input('')
+driver.close()
