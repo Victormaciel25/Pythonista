14c6ba28d3306d1bcc48cf38fdd6cf3304b6b4f6
Victor Maciel <victormaciel.dev@gmail.com>
2024-12-11 21:05:11 -0300

    Create baixarImagens.py

---
+++ selenium/baixarImagens.py
@@ -0,0 +1,46 @@
+from tabnanny import check
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+import random
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,1000', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app')
+sleep(1)
+driver.maximize_window()
+sleep(1)
+driver.execute_script('window.scrollTo(0,1500)')
+sleep(1)
+imagens = driver.find_elements(By. XPATH, "//img[@class='img-thumbnail']")
+sleep(1)
+contador = 1
+
+for imagem in imagens:
+    with open(f'imagem{contador}.png', 'wb') as arquivo:
+        arquivo.write(imagem.screenshot_as_png)
+        sleep(1)
+    contador += 1
+
+input('')
+driver.close()
