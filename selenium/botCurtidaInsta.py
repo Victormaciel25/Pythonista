43f2c63b3b4947667b7f623808bdc2d0a0e6fbb9
Victor Maciel <victormaciel.dev@gmail.com>
2024-12-16 18:58:24 -0300

    Update botCurtidaInsta.py

--- selenium/botCurtidaInsta.py
+++ selenium/botCurtidaInsta.py
@@ -4,7 +4,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import *
+from selenium.webdriver.support import expected_conditions as condicao_esperada
+from loginesenha import login,senha
 
 def iniciar_driver():
     chrome_options = Options()
@@ -20,15 +23,30 @@ def iniciar_driver():
     })
     driver = webdriver.Chrome(service=ChromeService(
         ChromeDriverManager().install()), options=chrome_options)
+    
+    wait = WebDriverWait(
+        driver,
+        10,
+        poll_frequency=1,
+        ignored_exceptions=[
+            NoSuchElementException,
+            ElementNotVisibleException,
+            ElementNotSelectableException,
+        ]
+    )
 
-    return driver
+    return driver, wait
 
 
 driver = iniciar_driver()
 # Entrar no site do instagram
 driver.get('https://www.instagram.com/')
 # Clicar e editar o usuario
-driver.
+campo_usuario = wait.until(condicao_esperada.element_to_be_clickable((By.XPATH, '//input[@name="username"]')))
+campo_usuario.send_keys(login)
+
+
+
 
 
 input('')
