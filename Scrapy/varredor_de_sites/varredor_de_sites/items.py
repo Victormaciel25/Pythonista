f2fb73f78c519e17eb40f4747002b6aff1066a09
Victor Maciel <victormaciel.dev@gmail.com>
2024-12-23 16:13:55 -0300

    up

--- Scrapy/varredor_de_sites/varredor_de_sites/items.py
+++ Scrapy/varredor_de_sites/varredor_de_sites/items.py
@@ -4,9 +4,32 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst, Join
 
+def tirar_espaco_em_branco(valor):
+    return valor.strip()
 
-class VarredorDeSitesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+def botar_maiusculo(valor):
+    return valor.upper()
+
+def processar_carateres_especiais(valor):
+    return valor.replace(u"\u2019", '')
+
+def mudar_virgula_por_ponto_evirgula(valor):
+    return valor.replace(',', ';')
+
+class CitacaoItem(scrapy.Item):
+    frase = scrapy.Field(
+        input_processor=MapCompose(
+            tirar_espaco_em_branco, processar_carateres_especiais),
+        output_processor=TakeFirst()
+    )
+    autor = scrapy.Field(
+        input_processor=MapCompose(tirar_espaco_em_branco, botar_maiusculo),
+        output_processor=TakeFirst()
+    )
+    tags = scrapy.Field(
+        output_processor=Join(';')
+    )
+
+     
--- Scrapy/varredor_de_sites/varredor_de_sites/spiders/goodreads.py
+++ Scrapy/varredor_de_sites/varredor_de_sites/spiders/goodreads.py
@@ -1,5 +1,7 @@
 import scrapy
-# CamelCase
+from scrapy.loader import ItemLoader
+from varredor_de_sites.items import CitacaoItem
+
 class GoodReadsSpider(scrapy.Spider):
     # Identidade
     name = 'quotebot'
@@ -8,25 +10,21 @@ class GoodReadsSpider(scrapy.Spider):
         urls = ['https://www.goodreads.com/quotes']
 
         for url in urls:
-            yield scrapy.Request(url=url,callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     # Response
     def parse(self, response):
         # Aqui é onde você deve processar o que é retornado da response
         for elemento in response.xpath("//div[@class='quote']"):
-            yield{
-                'frase':elemento.xpath(".//div[@class='quoteText']/text()").get(),
-                'autor':elemento.xpath(".//span[@class='authorOrTitle']/text()").get(),
-                'tags':elemento.xpath(".//div[@class='greyText smallText left']/a/text()").getall()
-            }
-        # Encontrar o link para a próxima página e extrair o número da próxima página
-        numero_proxima_pagina = response.xpath("//a[@class='next_page']/@href").get().split("=")[1]
-        print('#'*20)
-        print(numero_proxima_pagina)
-        print('#'*20)
-        if numero_proxima_pagina is not None:
-            link_proxima_pagina = f'https://www.goodreads.com/quotes?page={numero_proxima_pagina}'
-            print('#'*20)
-            print(link_proxima_pagina)
-            print('#'*20)
-            yield scrapy.Request(url=link_proxima_pagina,callback=self.parse)
+            loader = ItemLoader(item=CitacaoItem(),
+                                selector=elemento, response=response)
+            loader.add_xpath('frase', ".//div[@class='quoteText']/text()")
+            loader.add_xpath('autor', ".//span[@class='authorOrTitle']/text()")
+            loader.add_xpath('tags', ".//div[@class='greyText smallText left']/a/text()")
+            yield loader.load_item()
+
+        # Encontrar o link para a próxima página
+        next_page = response.xpath("//a[@class='next_page']/@href").get()
+        if next_page is not None:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
